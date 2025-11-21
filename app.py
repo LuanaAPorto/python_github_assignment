88944b4418d8bb9a75728419795f61f0d84b80ed
Luana Porto <luanaporto@usf.edu>
2025-11-21 16:04:25 -0500

    Add basic error handling for user input

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ hours = input("How many hours did you spend on your computer today?")
 hours = float(hours)
 weekly_hours = hours * 7 
 print(f"Your screen time this week was: {weekly_hours} hours, which exceeds recommended weekly screen time")
+
+try:
+    hours = float(hours)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
