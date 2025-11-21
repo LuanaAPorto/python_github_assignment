2f62f89ac1e0eaefdc24b98f0a7f0420b64200b6
Luana Porto <luanaporto@usf.edu>
2025-11-21 16:26:40 -0500

    removed "hours = input("How many hours did you spend on your phone today", and replaced it with individual questions for each day of the week

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 print("Welcome to my Python program")
-hours = input("How many hours did you spend on your computer today?")
+hoursSU = input("How many hours did you spend on your phone on Sunday?")
+hoursM = input("How many hours did you spend on your phone on Monday?")
+hoursT = input("How many hours did you spend on your phone on Tuesday?")
+hoursW = input("How many hours did you spend on your phone on Wednesday?")
+hoursTR = input("How many hours did you spend on your phone on Thursday?")
+hoursF = input("How many hours did you spend on your phone on Friday?")
+hoursS = input("How many hours did you spend on your phone on Saturday?")
+
 hours = float(hours)
-weekly_hours = hours * 7 
+
+
+weekly_hours = hours * 7
+
+
 print(f"Your screen time this week was: {weekly_hours} hours, which exceeds recommended weekly screen time")
+
+
 
 try:
     hours = float(hours)
